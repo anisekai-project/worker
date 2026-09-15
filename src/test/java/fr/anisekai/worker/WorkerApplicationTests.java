076a60da8f75fed9a14f19cf479d04aee49b0f7c
alexpado <contact@alexpado.fr>
2026-09-15 13:06:08 +0200

    Initial Commit

---
+++ src/test/java/fr/anisekai/worker/WorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.anisekai.worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
